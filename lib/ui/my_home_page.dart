20185e1497f01ebb1e6d42322243a4447a70f519
abumaryam <dianprawira@sisfo.untan.ac.id>
2023-10-08 16:08:32 +0700

    Pemrosesan input dari text field

--- lib/ui/my_home_page.dart
+++ lib/ui/my_home_page.dart
@@ -1,18 +1,52 @@
 import 'package:flutter/material.dart';
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   final String title;
   const MyHomePage({super.key, required this.title});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final namaController = TextEditingController();
+  String nama = '';
+
+  @override
+  void dispose() {
+    namaController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(widget.title),
       ),
-      body: const Center(
-        child: Text(
-          'Halo Semuanya!',
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: TextField(
+                controller: namaController,
+                decoration: const InputDecoration(
+                  hintText: 'Masukkan Nama Anda',
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  nama = "Halo ${namaController.text}";
+                });
+              },
+              child: const Text('Sapa'),
+            ),
+            Text(nama),
+          ],
         ),
       ),
     );
